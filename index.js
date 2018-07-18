5bd0030b984a99e70b5ac25aaf9dec2859ea80cd
Shaun Starsprung <shaun.starsprung@auth0.com>
2018-07-18 10:10:55 -0700

    Added password policies

---
+++ index.js
@@ -0,0 +1,38 @@
+const { charsets } = require('password-sheriff');
+
+const upperCase = charsets.upperCase;
+const lowerCase = charsets.lowerCase;
+const numbers = charsets.numbers;
+const specialCharacters = charsets.specialCharacters;
+
+const policies = {
+  none: {
+    length: { minLength: 1 }
+  },
+  low: {
+    length: { minLength: 6 }
+  },
+  fair: {
+    length: { minLength: 8 },
+    contains: {
+      expressions: [lowerCase, upperCase, numbers]
+    }
+  },
+  good: {
+    length: { minLength: 8 },
+    containsAtLeast: {
+      atLeast: 3,
+      expressions: [lowerCase, upperCase, numbers, specialCharacters]
+    }
+  },
+  excellent: {
+    length: { minLength: 10 },
+    containsAtLeast: {
+      atLeast: 3,
+      expressions: [lowerCase, upperCase, numbers, specialCharacters]
+    },
+    identicalChars: { max: 2 }
+  }
+};
+
+module.exports = policies;
